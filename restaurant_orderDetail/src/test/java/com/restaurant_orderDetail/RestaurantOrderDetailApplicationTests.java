76f57bc3f68110d704824fb1f5ff303122c55905
chen po-hsi <j0933473201@gmail.com>
2019-08-12 15:34:38 +0800

    777

---
+++ restaurant_orderDetail/src/test/java/com/restaurant_orderDetail/RestaurantOrderDetailApplicationTests.java
@@ -0,0 +1,16 @@
+package com.restaurant_orderDetail;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RestaurantOrderDetailApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
