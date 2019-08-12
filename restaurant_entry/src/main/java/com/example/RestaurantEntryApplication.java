5135c18413f6cfdac0f2836b9e7a896ee71a759b
tsai_wayne <wayne.tsai@thinkpower-info.com>
2019-08-12 13:43:42 +0800

    新增子專案 restaurant_entry，可統一用此專案執行，其他子專案之 @SpringBootApplication 標註類別可刪除。 application.properties 由子專案 restaurant_shared 管理，新增build.gradle設定，其他子專案之 application.properties 可刪除。

--- restaurant_entry/src/main/java/com/example/RestaurantEntryApplication.java
+++ restaurant_entry/src/main/java/com/example/RestaurantEntryApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestaurantMenuApplication {
-	
+public class RestaurantEntryApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(RestaurantMenuApplication.class, args);
+		SpringApplication.run(RestaurantEntryApplication.class, args);
 	}
+
 }
